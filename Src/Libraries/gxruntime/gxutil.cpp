aaf292263e394993fdadd45987aa2f307fa272b9
Juan Pablo Arce <juanjprot@gmail.com>
2018-08-05 22:13:51 -0300

    RIP stdutil

---
+++ Src/Libraries/gxruntime/gxutil.cpp
@@ -0,0 +1,48 @@
+
+#include "gxutil.h"
+
+#include <Windows.h>
+
+int atoi( String s ){
+	return atoi( s.cstr() );
+}
+
+double atof( String s ){
+	return atof( s.cstr() );
+}
+
+String itoa( int n ){
+	return String(n);
+}
+
+String ftoa( float n ){
+    String(n);
+}
+
+String tolower( String s ){
+	return s.toLower();
+}
+
+String toupper( String s ){
+	return s.toUpper();
+}
+
+String fullfilename( String t ){
+	char buff[MAX_PATH+1],*p;
+	GetFullPathName( t.cstr(),MAX_PATH,buff,&p );
+	return String(buff);
+}
+
+String filenamepath( String t ){
+	char buff[MAX_PATH+1],*p;
+	GetFullPathName( t.cstr(),MAX_PATH,buff,&p );
+	if( !p ) return "";
+	*p=0;return String(buff);
+}
+
+String filenamefile( String t ){
+	char buff[MAX_PATH+1],*p;
+	GetFullPathName( t.cstr(),MAX_PATH,buff,&p );
+	if( !p ) return "";
+	return String( p );
+}
---
+++ Src/Libraries/gxruntime/gxutil.h
@@ -0,0 +1,22 @@
+
+#ifndef STDUTIL_H
+#define STDUTIL_H
+
+#include <iostream>
+
+#include "StringType.h"
+
+//TODO: remove the stuff String already implements
+int atoi(String s );
+double atof(String s );
+String itoa( int n );
+String ftoa( float n );
+String tolower( String s );
+String toupper( String s );
+
+//keep this tho
+String fullfilename( String t );
+String filenamepath( String t );
+String filenamefile( String t );
+
+#endif
