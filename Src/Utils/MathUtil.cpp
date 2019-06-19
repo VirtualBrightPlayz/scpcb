d9db42446dad49c7ac5cd64ef0d67b22d68df163
juanjp600 <juanjprot@gmail.com>
2019-06-18 23:17:33 -0300

    Updated sln, fixed MathUtil.cpp

--- Src/Utils/MathUtil.cpp
+++ Src/Utils/MathUtil.cpp
@@ -1,7 +1,8 @@
-#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 #include "MathUtil.h"
 
 float MathUtil::degToRad(float degree) {
-    return degree * M_PI / 180;
+    return degree * M_PI / 180.0f;
 }
