28f1a4e00421f8930ade24710233110d8cd2f744
CommanderMark <commanderpro100@gmail.com>
2019-07-19 04:06:11 -0400

    Indenting.

--- Src/Menus/PauseMenu.h
+++ Src/Menus/PauseMenu.h
@@ -6,55 +6,55 @@
 #include "GUI/GUIFrame.h"
 
 class PauseMenu {
-public:
-    enum class SubState {
-        Main,
-        NewGame,
-        LoadGame,
-        Options,
-        Quitting,
-        Hidden
-    };
+    public:
+        enum class SubState {
+            Main,
+            NewGame,
+            LoadGame,
+            Options,
+            Quitting,
+            Hidden
+        };
 
-private:
-    // Whether the pause menu is being shown on startup as oppose to in the middle of a game.
-    bool isMainMenu;
+    private:
+        // Whether the pause menu is being shown on startup as oppose to in the middle of a game.
+        bool isMainMenu;
 
-    enum class OptionsTab {
-        Graphics,
-        Audio,
-        Controls,
-        Gameplay
-    };
+        enum class OptionsTab {
+            Graphics,
+            Audio,
+            Controls,
+            Gameplay
+        };
 
-    SubState currState;
-    OptionsTab currOptionsTab;
+        SubState currState;
+        OptionsTab currOptionsTab;
 
-    void setOptionsTab(OptionsTab tab);
+        void setOptionsTab(OptionsTab tab);
 
-    // --Buttons.
+        // --Buttons.
 
-    // Main state.
-    GUIButton* newgame;
-    GUIButton* loadgame;
-    GUIButton* options;
-    GUIButton* quit;
+        // Main state.
+        GUIButton* newgame;
+        GUIButton* loadgame;
+        GUIButton* options;
+        GUIButton* quit;
 
-    // Quitting state.
-    GUIFrame* quitFrame;
-    GUIButton* quitYes;
-    GUIButton* quitNo;
+        // Quitting state.
+        GUIFrame* quitFrame;
+        GUIButton* quitYes;
+        GUIButton* quitNo;
 
-    UIMesh* uiMesh;
-    KeyBinds* keyBinds;
+        UIMesh* uiMesh;
+        KeyBinds* keyBinds;
 
-public:
-    PauseMenu(UIMesh* um, KeyBinds* kb);
+    public:
+        PauseMenu(UIMesh* um, KeyBinds* kb);
 
-    void setState(SubState state);
+        void setState(SubState state);
 
-    void update(World* world, PGE::Vector2f mousePosition);
-    void render();
+        void update(World* world, PGE::Vector2f mousePosition);
+        void render();
 };
 
 #endif // PAUSEMENU_H_INCLUDED
