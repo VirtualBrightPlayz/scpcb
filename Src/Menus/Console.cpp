a81622ad24efaf786ada92c8d49882a249e86617
QuaternionMark <markjarjour@gmail.com>
2020-07-12 18:30:46 -0400

    Console test.

--- Src/Menus/Console.cpp
+++ Src/Menus/Console.cpp
@@ -16,10 +16,30 @@ Console::Console(World* wrld, UIMesh* um, Font* font, KeyBinds* kb, Config* con,
 
     uiMesh = um;
     this->font = font;
+    this->keyBinds = kb;
+
+    registerInternalCommands();
+}
+
+Console::~Console() {
+    for (int i = 0; i < (int)interCommands.size(); i++) {
+        delete interCommands[i];
+    }
 }
 
 void Console::update(const PGE::Vector2f& mousePosition) {
     input->update(mousePosition);
+
+    if (keyBinds->enter->isHit()) {
+        std::vector<PGE::String> vect = input->getText().split(" ", true);
+        if (vect.size() > 0) {
+            PGE::String command = vect[0];
+            vect.erase(vect.begin());
+
+            executeCommand(command, vect);
+            //input->setText("");
+        }
+    }
 }
 
 void Console::render() const {
@@ -29,4 +49,35 @@ void Console::render() const {
     input->render();
 
     uiMesh->endRender();
+}
+
+void Console::executeCommand(const PGE::String& com, const std::vector<PGE::String>& params) {
+    for (int i = 0; i < (int)interCommands.size(); i++) {
+        if (com.equals(interCommands[i]->getName())) {
+            interCommands[i]->execute(this, params);
+        }
+    }
+}
+
+void Console::todo_test() {
+    input->setText("hey gotem");
+}
+
+class TestCommand : public InternalCommand {
+public:
+    PGE::String getName() const override {
+        return "test";
+    }
+
+    PGE::String getHelpText() const override {
+        return "AAAAAAAAAAAAA";
+    }
+
+    void execute(Console* console, const std::vector<PGE::String>& params) const override {
+        console->todo_test();
+    }
+};
+
+void Console::registerInternalCommands() {
+    interCommands.push_back(new TestCommand());
 }
