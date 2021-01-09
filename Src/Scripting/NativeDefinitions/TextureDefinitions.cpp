ffad1b9da337297f91b0fa13948ece76e4ca94e2
Saalvage <29021710+Saalvage@users.noreply.github.com>
2021-01-09 03:10:50 +0100

    Exposed copy method of textures.

--- Src/Scripting/NativeDefinitions/TextureDefinitions.cpp
+++ Src/Scripting/NativeDefinitions/TextureDefinitions.cpp
@@ -4,10 +4,15 @@
 
 #include "../../Graphics/GraphicsResources.h"
 
+static PGE::Texture* copyTexture(PGE::Texture* texture) {
+    return texture->copy();
+}
+
 TextureDefinitions::TextureDefinitions(ScriptManager* mgr, GraphicsResources* gr) {
     engine = mgr->getAngelScriptEngine();
 
     engine->RegisterObjectType("Texture", sizeof(PGE::Texture), asOBJ_REF | asOBJ_NOCOUNT);
+    engine->RegisterObjectMethod("Texture", "Texture@ copy()", asFUNCTION(copyTexture), asCALL_CDECL_OBJLAST);
 
     engine->SetDefaultNamespace("Texture");
     engine->RegisterGlobalFunction("Texture@ get(const string&in filename)", asMETHOD(GraphicsResources, getTexture), asCALL_THISCALL_ASGLOBAL, gr);
