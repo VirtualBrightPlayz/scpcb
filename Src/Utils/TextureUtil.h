01f9a10399270cbe52e10c289d9b17b580e3b10c
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-05-16 22:07:18 +0200

    Remove redundant semicolon

--- Src/Utils/TextureUtil.h
+++ Src/Utils/TextureUtil.h
@@ -15,6 +15,6 @@ class ImageData : public PGE::Resource<PGE::byte*> {
 namespace TextureHelper {
 	PGE::Texture* load(PGE::Graphics* gfx, const PGE::FilePath& file);
 	PGE::Texture* load(PGE::Graphics* gfx, PGE::byte* fileData, int size);
-};
+}
 
 #endif // TEXTURE_UTIL_H_DEFINED
