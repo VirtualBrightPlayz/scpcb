2eabd37d4198ca8a4bfdc42fb2d5c34326c3f2bb
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-05-27 11:49:58 +0200

    Apply PGE changes

--- Src/Scripting/NativeDefinitions/ColorDefinitions.cpp
+++ Src/Scripting/NativeDefinitions/ColorDefinitions.cpp
@@ -12,7 +12,7 @@ static void colorConstructorParametrized(float r, float g, float b, float a, voi
     new(memory) PGE::Color(r, g, b, a);
 }
 
-static void colorConstructorParametrizedInt(int r, int g, int b, int a, void* memory) {
+static void colorConstructorParametrizedInt(PGE::byte r, PGE::byte g, PGE::byte b, PGE::byte a, void* memory) {
     new(memory) PGE::Color(r, g, b, a);
 }
 
@@ -26,7 +26,7 @@ ColorDefinitions::ColorDefinitions(ScriptManager* mgr) {
     engine->RegisterObjectType("Color", sizeof(PGE::Color), asOBJ_VALUE | asOBJ_APP_CLASS_ALLFLOATS | asGetTypeTraits<PGE::Color>());
     engine->RegisterObjectBehaviour("Color", asBEHAVE_CONSTRUCT, "void f()", asFUNCTION(colorConstructor), asCALL_CDECL_OBJLAST);
     engine->RegisterObjectBehaviour("Color", asBEHAVE_CONSTRUCT, "void f(float r, float g, float b, float a=1.0)", asFUNCTION(colorConstructorParametrized), asCALL_CDECL_OBJLAST);
-    engine->RegisterObjectBehaviour("Color", asBEHAVE_CONSTRUCT, "void f(int r, int g, int b, int a=1.0)", asFUNCTION(colorConstructorParametrizedInt), asCALL_CDECL_OBJLAST);
+    engine->RegisterObjectBehaviour("Color", asBEHAVE_CONSTRUCT, "void f(uint8 r, uint8 g, uint8 b, uint8 a=1.0)", asFUNCTION(colorConstructorParametrizedInt), asCALL_CDECL_OBJLAST);
     engine->RegisterObjectBehaviour("Color", asBEHAVE_DESTRUCT, "void f()", asFUNCTION(colorDestructor), asCALL_CDECL_OBJLAST);
 
     engine->RegisterObjectMethod("Color", "bool opEquals(const Color&in other) const", asMETHODPR(PGE::Color, operator==, (const PGE::Color&) const, bool), asCALL_THISCALL);
@@ -36,14 +36,14 @@ ColorDefinitions::ColorDefinitions(ScriptManager* mgr) {
     engine->RegisterObjectProperty("Color", "float blue", asOFFSET(PGE::Color, blue));
     engine->RegisterObjectProperty("Color", "float alpha", asOFFSET(PGE::Color, alpha));
 
-    engine->RegisterObjectMethod("Color", "int get_redInt() property", asMETHOD(PGE::Color, getRedInt), asCALL_THISCALL);
-    engine->RegisterObjectMethod("Color", "int get_greenInt() property", asMETHOD(PGE::Color, getGreenInt), asCALL_THISCALL);
-    engine->RegisterObjectMethod("Color", "int get_blueInt() property", asMETHOD(PGE::Color, getBlueInt), asCALL_THISCALL);
-    engine->RegisterObjectMethod("Color", "int get_alphaInt() property", asMETHOD(PGE::Color, getAlphaInt), asCALL_THISCALL);
-    engine->RegisterObjectMethod("Color", "void set_redInt(int r) property", asMETHOD(PGE::Color, setRedInt), asCALL_THISCALL);
-    engine->RegisterObjectMethod("Color", "void set_greenInt(int g) property", asMETHOD(PGE::Color, setGreenInt), asCALL_THISCALL);
-    engine->RegisterObjectMethod("Color", "void set_blueInt(int b) property", asMETHOD(PGE::Color, setBlueInt), asCALL_THISCALL);
-    engine->RegisterObjectMethod("Color", "void set_alphaInt(int a) property", asMETHOD(PGE::Color, setAlphaInt), asCALL_THISCALL);
+    engine->RegisterObjectMethod("Color", "uint8 get_redByte() property", asMETHOD(PGE::Color, getRed), asCALL_THISCALL);
+    engine->RegisterObjectMethod("Color", "uint8 get_greenByte() property", asMETHOD(PGE::Color, getGreen), asCALL_THISCALL);
+    engine->RegisterObjectMethod("Color", "uint8 get_blueByte() property", asMETHOD(PGE::Color, getBlue), asCALL_THISCALL);
+    engine->RegisterObjectMethod("Color", "uint8 get_alphaByte() property", asMETHOD(PGE::Color, getAlpha), asCALL_THISCALL);
+    engine->RegisterObjectMethod("Color", "void set_redInt(uint8 r) property", asMETHOD(PGE::Color, setRed), asCALL_THISCALL);
+    engine->RegisterObjectMethod("Color", "void set_greenInt(uint8 g) property", asMETHOD(PGE::Color, setGreen), asCALL_THISCALL);
+    engine->RegisterObjectMethod("Color", "void set_blueInt(uint8 b) property", asMETHOD(PGE::Color, setBlue), asCALL_THISCALL);
+    engine->RegisterObjectMethod("Color", "void set_alphaInt(uint8 a) property", asMETHOD(PGE::Color, setAlpha), asCALL_THISCALL);
 
     engine->RegisterObjectMethod("Color", "Color& opAssign(const Color &in other)", asMETHODPR(PGE::Color, operator=, (const PGE::Color&), PGE::Color&), asCALL_THISCALL);
 
