7d4cfd99a92ebed6d569b662aaf829d5f8f90f71
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-07-19 04:50:43 +0200

    """Fixed""" menu orientation

--- Src/Graphics/GraphicsResources.cpp
+++ Src/Graphics/GraphicsResources.cpp
@@ -142,7 +142,8 @@ void GraphicsResources::updateOrthoMat(float aspectRatio) {
     float h = 100.f;
     float nearZ = 0.01f;
     float farZ = 1.f;
-    orthoMat = PGE::Matrix4x4f::constructOrthographicMat(w, h, nearZ, -farZ);
+    // TODO: I really don't fucking know.
+    orthoMat = PGE::Matrix4x4f::constructOrthographicMat(-w, -h, nearZ, -farZ);
 }
 
 PGE::Matrix4x4f GraphicsResources::getOrthoMat() const {
