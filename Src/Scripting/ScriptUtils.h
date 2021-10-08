73cd01235634404440360d5b96ab2a6187b1c135
VirtualBrightPlayz <lego.trains.b@gmail.com>
2021-10-08 16:52:30 -0700

    fix win32

--- Src/Scripting/ScriptUtils.h
+++ Src/Scripting/ScriptUtils.h
@@ -1,6 +1,12 @@
 #ifndef SCRIPT_UTILS_H_INCLUDED
 #define SCRIPT_UTILS_H_INCLUDED
 
+#include <angelscript.h>
+
+#ifdef _WIN32
+#define asVIRTUAL_METHOD(c,m) asMETHOD(c,m)
+#else
 #define asVIRTUAL_METHOD(c,m) asSMethodPtr<sizeof(&c::m)>::Convert((void*)(&c::m))
+#endif
 
 #endif // SCRIPT_UTILS_H_INCLUDED
